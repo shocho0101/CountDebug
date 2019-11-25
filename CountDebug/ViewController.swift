b353a8856df2263631818f20ed5ce32a3bd643f6
choccho <shocho0101@hotmail.co.jp>
2019-11-25 13:42:24 +0900

    modify

--- CountDebug/ViewController.swift
+++ CountDebug/ViewController.swift
@@ -9,10 +9,41 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var label: UILabel!
+    
+    var count: Int = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func plus() {
+        count += 1
+        label.text = String(count)
+        updateLabel()
+    }
+    
+    @IBAction func minus() {
+        count -= 1
+        label.text = String(count)
+        updateLabel()
+    }
+    
+    func updateLabel() {
+        if count >= 100 {
+            label.textColor = .red
+        }
+        
+        if count >= 0 {
+            label.textColor = .black
+        }
+        
+        if count < 0 {
+            label.textColor = .blue
+            count -= 1
+        }
     }
 
 
